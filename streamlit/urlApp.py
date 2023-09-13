55bbc36860ffb1176b925406cc532636080dde08
Kenneth Gibb Wraight <kenneth.gibb.wraight@cern.ch>
2023-09-13 10:23:25 +0100

    button

--- streamlit/urlApp.py
+++ streamlit/urlApp.py
@@ -5,7 +5,8 @@ from datetime import datetime
 st.title("Welcome to the URL app")
 st.write("Intended to test running a _streamlit_ app using URL")
 
-st.write("__Timecheck__:", datetime.now())
+if st.button("Time check"):
+    st.write("Het is:", datetime.now())
 
 if st.checkbox("embedded?"):
     components.iframe("https://docs.streamlit.io/library/components/components-api", width=800, height=600, scrolling=True)
