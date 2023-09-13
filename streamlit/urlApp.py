281e364daef2164b91b6ae0a6ce8ed61ca98db6c
Kenneth Gibb Wraight <kenneth.gibb.wraight@cern.ch>
2023-09-13 10:06:01 +0100

    Update urlApp.py

--- streamlit/urlApp.py
+++ streamlit/urlApp.py
@@ -4,4 +4,8 @@ import streamlit.components.v1 as components
 st.title("Welcome to the URL app")
 st.write("Intended to test running a _streamlit_ app using URL")
 
-components.iframe("https://docs.streamlit.io/library/components/components-api")
+if st.checkbox("embedded?"):
+    components.iframe("https://docs.streamlit.io/library/components/components-api")
+
+
+st.write("Tot ziens!")
