56babbaf8f0c9549c45a8d9edb1a0687ec83eb1d
Kenneth Gibb Wraight <kenneth.gibb.wraight@cern.ch>
2023-09-13 10:08:33 +0100

    scrolling

--- streamlit/urlApp.py
+++ streamlit/urlApp.py
@@ -5,7 +5,7 @@ st.title("Welcome to the URL app")
 st.write("Intended to test running a _streamlit_ app using URL")
 
 if st.checkbox("embedded?"):
-    components.iframe("https://docs.streamlit.io/library/components/components-api")
+    components.iframe("https://docs.streamlit.io/library/components/components-api", width=800, height=600, scrolling=True)
 
 
 st.write("Tot ziens!")
